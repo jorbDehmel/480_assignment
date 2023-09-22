45fdb6ba552882f12774ef088fd9c35c6c906e61
Jorb Dehmel <jdehmel@outlook.com>
2023-09-22 16:41:12 -0600

    Added stuff to sort_requirements for non-comparison based sorts

---
+++ radix.hpp
@@ -0,0 +1,16 @@
+// Jordan does this one
+
+#ifndef RADIX_HPP
+#define RADIX_HPP
+
+#include "sort_requirements.hpp"
+
+/*
+Since radix sort is not compare based, I
+have chosen to use
+*/
+
+// Recursive
+sort_data radixSort(unsigned long long *array, const unsigned long long size);
+
+#endif
--- sort_requirements.hpp
+++ sort_requirements.hpp
@@ -15,15 +15,29 @@ using namespace std;
 // Each sorting algorithm should return one of these
 struct sort_data
 {
+    // Attributes of all sorts
+    string name = "";
     unsigned long long execution_ns = 0;
-    unsigned long long comparisons = 0;
-    unsigned long long swaps = 0;
-    unsigned long long bytes_used = 0;
-
     bool is_stable = false;
     bool is_in_place = false;
+    bool is_comparison_based = true;
 
-    string name = "";
+    // For comparison-based sorts
+    unsigned long long comparisons = 0;
+    unsigned long long swaps = 0;
+
+    // For non-comparison sorts
+    unsigned long long array_accesses = 0;
+
+    /*
+    If you allocate 15 100-byte arrays,
+    deleting the previous before making
+    the next, the total bytes used is
+    1500, while the maximum concurrent
+    bytes used is 100.
+    */
+    unsigned long long total_bytes_used = 0;
+    unsigned long long max_concurrent_bytes_used = 0;
 };
 
 // Compare two objects and update the associated sort_data
